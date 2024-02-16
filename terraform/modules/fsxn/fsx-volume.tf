bd3aecfcca83b36e83ad68a067ff2e2fcd294771
Varun Seth <1327311+varunrai@users.noreply.github.com>
2024-02-17 07:35:17 +0800

    added the code for creating separate log and data volumes. added local variable for sql EC2 instance name

--- terraform/modules/fsxn/fsx-volume.tf
+++ terraform/modules/fsxn/fsx-volume.tf
@@ -1,13 +1,25 @@
-resource "aws_fsx_ontap_volume" "fsxvol01" {
-  name                       = "vol1"
-  junction_path              = "/vol1"
-  security_style             = var.fsxn_volume_security_style
-  size_in_megabytes          = 1280000
+resource "aws_fsx_ontap_volume" "fsxn_sql_data_volume" {
+  name                       = "${local.server_name}_data"
+  junction_path              = "/${local.server_name}_data"
+  security_style             = "NTFS"
+  size_in_megabytes          = 10240000
   storage_efficiency_enabled = true
   storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.fsxsvm01.id
-
+  skip_final_backup          = true
   tiering_policy {
-    name           = "AUTO"
-    cooling_period = 2
+    name = "SNAPSHOT_ONLY"
+  }
+}
+
+resource "aws_fsx_ontap_volume" "fsxn_sql_log_volume" {
+  name                       = "${local.server_name}_log"
+  junction_path              = "/${local.server_name}_log"
+  security_style             = "NTFS"
+  size_in_megabytes          = 10240000
+  storage_efficiency_enabled = true
+  storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.fsxsvm01.id
+  skip_final_backup          = true
+  tiering_policy {
+    name = "SNAPSHOT_ONLY"
   }
 }
